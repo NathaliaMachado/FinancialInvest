fcd9d985cf2386a7db5e07b96706f83b331aa3e9
NathaliaMachado <nathaliamachd@gmail.com>
2019-09-04 11:25:54 -0400

    sum function

--- client/js/app/controllers/BusinessController.js
+++ client/js/app/controllers/BusinessController.js
@@ -7,12 +7,15 @@ class BusinessController {
         this._inputValor = $('#valor');
         this._form = $('.form');
         this._listBusiness = new ListBusiness();
+        this._businessView = new BusinessView($('#businessView'));
+        this._businessView.update(this._listBusiness);
     }
 
     adiciona(event) {
         event.preventDefault();
 
         this._listBusiness.adiciona(this._createBusiness());
+        this._businessView.update(this._listBusiness);
         console.log(this._listBusiness.business);
         this._cleanForm();
     }
